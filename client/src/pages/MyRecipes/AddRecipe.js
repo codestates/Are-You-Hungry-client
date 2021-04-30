de64c4b0fbfd13abb1cec551fcb9c4d0e85b2f5d
Hyun Don Moon <hyundonmoon@gmail.com>
2021-04-30 12:53:04 +0900

    Set up basic scaffolding for project client-side

---
+++ client/src/pages/MyRecipes/AddRecipe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "../../components/SideBar";
+
+const AddRecipe = () => {
+  return (
+    <>
+      <SideBar />
+      {/* <AddRecipe /> Need to fill in with actual HTML */}
+    </>
+  );
+};
+
+export default AddRecipe;
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+
+const SignUp = () => {
+  return (
+    <>
+      <SideBar />
+      {/* <SignUp /> Need to fill in with actual HTML */}
+    </>
+  );
+};
+
+export default SignUP;
