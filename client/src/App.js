e773f3252850a7f81a856c708546a7de57d99616
Hyun Don Moon <hyundonmoon@gmail.com>
2021-04-30 15:18:25 +0900

    Created SignUp page and styled it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <SignIn />
+      <SignUp />
     </>
   );
 };
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,13 +1,38 @@
 import React from "react";
 import SideBar from "../components/SideBar";
+import "../styles/SignIn.css";
 
 const SignUp = () => {
   return (
     <>
-      <SideBar />
-      {/* <SignUp /> Need to fill in with actual HTML */}
+      <SideBar isLoggedIn={false} />
+      <main class="main">
+        <form className="signup-form">
+          <h2>Sign Up</h2>
+          <div className="input-field">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" required />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" required />
+          </div>
+          <div className="input-field">
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="input-field">
+            <label htmlFor="phone">Phone:</label>
+            <input type="tel" id="phone" required />
+          </div>
+          <div className="button">
+            <input type="submit" value="Sign Up" />
+          </div>
+          <a href="#">Already have an account?</a>
+        </form>
+      </main>
     </>
   );
 };
 
-export default SignUP;
+export default SignUp;
